284773a3c9702cfccf084a5492e4c40e6f4c36be
Roland Tritsch <roland@tritsch.org>
2018-08-02 17:21:15 +0100

    Chapter01: Done.

--- src/Chapter01.hs
+++ src/Chapter01.hs
@@ -2,4 +2,21 @@
 module Chapter01 where
 
 doubleMe :: Int -> Int
-doubleMe x = x * x
+doubleMe x = x + x
+
+doubleUs :: Int -> Int -> Int
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber :: Int -> Int
+doubleSmallNumber x = if x < 100 then doubleMe x else x
+
+doubleSmallNumber' :: Int -> Int
+doubleSmallNumber' x = doubleSmallNumber x + 1
+
+first5Numbers = [0, 1, 2, 3, 4]
+next5Numbers = [x | x <- [5..9]]
+
+findRightTriangle maxLength perimeter = do
+  let sideLength = [1..maxLength]
+  let triples = [(a, b, c) | c <- sideLength, a <- [1..c], b <- [1..a], a^2 + b^2 == c^2, a + b + c == perimeter]
+  triples
--- test/Chapter01Spec.hs
+++ test/Chapter01Spec.hs
@@ -9,4 +9,27 @@ run :: IO ()
 run = hspec $ do
   describe "Chapter01" $ do
     it "should return the right result(s)" $ do
-      Chapter01.doubleMe 5 `shouldBe` 25
+      Chapter01.doubleMe 5 `shouldBe` 10
+      Chapter01.doubleUs 5 2 `shouldBe` 14
+      Chapter01.doubleSmallNumber 90 `shouldBe` 180
+      Chapter01.doubleSmallNumber 100 `shouldBe` 100
+      Chapter01.doubleSmallNumber' 90 `shouldBe` 181
+      Chapter01.doubleSmallNumber' 100 `shouldBe` 101
+      Chapter01.first5Numbers ++ Chapter01.next5Numbers `shouldBe` [0..9]
+      Chapter01.first5Numbers ++ [5] `shouldBe` [0..5]
+      4 : Chapter01.next5Numbers `shouldBe` [4..9]
+      Chapter01.first5Numbers !! 0 `shouldBe` 0
+      head Chapter01.first5Numbers `shouldBe` 0
+      tail Chapter01.first5Numbers `shouldBe` [1..4]
+      init Chapter01.first5Numbers `shouldBe` [0..3]
+      last Chapter01.first5Numbers `shouldBe` 4
+      length Chapter01.first5Numbers `shouldBe` 5
+      null Chapter01.first5Numbers `shouldBe` False
+      reverse Chapter01.first5Numbers `shouldBe` [4, 3..0]
+      take 1 Chapter01.first5Numbers `shouldBe` [head Chapter01.first5Numbers]
+      drop 1 Chapter01.first5Numbers `shouldBe` tail Chapter01.first5Numbers
+      minimum Chapter01.first5Numbers `shouldBe` head Chapter01.first5Numbers
+      maximum Chapter01.first5Numbers `shouldBe` last Chapter01.first5Numbers
+      elem (head Chapter01.first5Numbers) Chapter01.first5Numbers `shouldBe` True
+      elem (succ (last Chapter01.first5Numbers)) Chapter01.first5Numbers `shouldBe` False
+      Chapter01.findRightTriangle 10 24 `shouldBe` [(8, 6, 10)]
