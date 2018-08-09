f913063b940f5c9334f302d2f2283e20d6168e54
Roland Tritsch <roland@tritsch.org>
2018-08-09 17:04:04 +0100

    Day00: Add type concepts.

--- src/Chapter00.hs
+++ src/Chapter00.hs
@@ -3,3 +3,25 @@ module Chapter00 where
 
 hello :: String
 hello = "Hello World"
+
+data Point = Point { -- define new data type Point (structure of data)
+  x :: Int,
+  y :: Int
+} deriving (Eq) -- Point is an instance of type class Eq
+
+class (Eq a) => YesNo a where -- define new type class YesNo (behavior of data)
+  yesno :: a -> Bool
+
+instance YesNo Point where -- Point is an instance of type class YesNo (link behavior to data)
+  yesno (Point 0 0) = False
+  yesno (Point _ _) = True
+
+testIt :: Point -> Bool
+testIt p = yesno p
+
+compareIt :: Point -> Point -> Bool
+compareIt p p' = p == p'
+
+type Text = String -- Text is a type alias/synonym for String
+
+newtype Pair b a = Pair { getPair :: (a, b) }
